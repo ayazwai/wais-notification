b0f13317f8a527e9ab66df0332a0abde37c9529f
ayazwai <64478381+ayazwai@users.noreply.github.com>
2022-02-07 18:42:13 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -3,7 +3,7 @@ AddEventHandler('wais:sendNotif', function(type, title, message, duration)
     if not (type == "info" or type == "warning" or type == "error" or type == "success") then return end
 	if title == "" then return end
 	if message == "" then return end
-	if tonumber(duration) > 15000 then
+	if tonumber(duration) > 20000 then
 		print('15 Saniyeden büyük değer girmeyiniz')
 		return
 	end
